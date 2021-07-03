aead90e3c210a3eea07cb7b6c39d67bf127b0b81
Saurabh Tripathi <saurabhtripathi5122gmail.com>
2021-07-03 23:54:14 +0530

    for loops understood

---
+++ for_loops.java
@@ -0,0 +1,14 @@
+
+
+public class for_loops 
+{
+
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println("Saurabh");
+        }
+    }
+    
+}
