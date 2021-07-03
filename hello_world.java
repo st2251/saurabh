fe0e721451cbc3121f722bd65825dfc95f59f783
Saurabh Tripathi <saurabhtripathi5122gmail.com>
2021-07-04 02:46:37 +0530

    Resolved master head

--- hello_world.java
+++ hello_world.java
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
-/**
- * hello_world
- */
 public class hello_world {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-    }
-=======
-public class hello_world
- {
-     public static void main(String[] args) 
-     {
          System.out.println("Hello java world");
          System.out.println("in dev branch");      
      } 
-         
-     
-
-    
->>>>>>> dev
 }
